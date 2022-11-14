cd8966de91f35e7d1a9dd30ccd2c5a06bd0cf39b
DradarkTV <dradarkx@gmail.com>
2022-11-14 22:19:46 +0100

    Loading Screen

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import "./default.scss"
+import Loading from "./compenent/loading";
 
 const app = document.getElementById('app');
 
 let content = "";
-
+content += Loading();
 app.innerHTML = content;
