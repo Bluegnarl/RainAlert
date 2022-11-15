b88961d69a1803ef44c65e27a443321e412f31d5
DradarkTV <dradarkx@gmail.com>
2022-11-15 12:18:13 +0100

    Création Header et Footer

--- src/compenent/home.js
+++ src/compenent/home.js
@@ -2,7 +2,12 @@ const Home = () => {
     return(
     `
     <div class="home-screen">
-
+        <header>
+            <div class="logo-home"></div>
+            <div class="titre-home">RainAlert</div>
+        </header>
+        <div class="body-home"></div>
+        <footer></footer>
     </div>
     `
     )
