9a3efdf1d87b10b4889f58c973abb72a405d668f
DradarkTV <dradarkx@gmail.com>
2022-11-17 09:12:30 +0100

    Search Bar Amelioration

--- src/compenent/home.js
+++ src/compenent/home.js
@@ -7,11 +7,7 @@ const Home = () => {
             <div class="titre-home">RainAlert</div>
         </header>
         <div class="body-home">
-            <div class="search-bar">
-                <div class="plane-search-bar"></div>
-                <div class="text-search-bar">Verviers, BE</div>
-                <div class="logo-search-bar"></div>
-            </div>
+            <input class="search-bar" placeholder="Rechercher">
         </div>
         <footer></footer>
     </div>
