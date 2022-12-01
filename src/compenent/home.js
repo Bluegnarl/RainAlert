36192b6626a5058df2db28c45bc7c9b688328a42
DradarkTV <dradarkx@gmail.com>
2022-12-01 16:00:03 +0100

    ajout carte, time, temperature

--- src/compenent/home.js
+++ src/compenent/home.js
@@ -11,6 +11,14 @@ const Home = () => {
                         <div class="rechercher">Rechercher</div>
                     </div>
                 </div>
+                <div class="meteo">
+                    <div class="logo-meteo">
+                        <div class="logo-meteo-1"></div>
+                    </div>
+                    <div class="text-meteo">
+                        <div class="text-meteo-1">12°C</div>
+                    </div>
+                </div>
                 <div class="graph">
                     <div class="chart-top">
                         <div class="chart-precip">Précipitations</div>
@@ -41,8 +49,17 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
+                <div class="the-time">
+                    <div class="time-1">15:49</div>
+                    <div class="time-2">16:00</div>
+                    <div class="time-3">17:00</div>
+                    <div class="time-4">18:00</div>
+                    <div class="time-5">19:00</div>
+                </div>
             </div>
-            <div class="map-home"></div>
+            <div class="map-home">
+                <div class="google-map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1672521.215237847!2d1.65653915762622!3d46.49012643934689!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd54a02933785731%3A0x6bfd3f96c747d9f7!2sFrance!5e0!3m2!1sfr!2sbe!4v1669900972368!5m2!1sfr!2sbe" style="border:0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+            </div>
         </div>
     `
     )
